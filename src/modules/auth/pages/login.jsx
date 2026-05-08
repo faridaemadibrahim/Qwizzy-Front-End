5e14de3e25934cdd81d6b0b2814122f42fccbf68
Farida Emad <far.emad888@gmail.com>
2026-05-08 23:05:57 +0300

    fix vercel error

--- src/modules/auth/pages/login.jsx
+++ src/modules/auth/pages/login.jsx
@@ -1,13 +1,14 @@
-import '../styles/auth.css'
+import "../styles/auth.css";
 
-import LoginCard from '../components/LoginCard.jsx'
+import LoginCard from "../components/LoginCard.jsx";
 
 export default function Login() {
   return (
+    //fix
     <div className="auth-page">
       <main className="auth-main">
         <LoginCard />
       </main>
     </div>
-  )
+  );
 }
